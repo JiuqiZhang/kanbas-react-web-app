1db9057e01844434c6640c0cea84952f4eeac92b
JiuqiZhang <jz3337@nyu.edu>
2024-12-10 01:40:57 -0800

    a6

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,71 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import * as client from "./Courses/client";
+import * as userClient from "./Account/client";
+
 export default function Dashboard(
-    { courses, course, setCourse, addNewCourse,
-        deleteCourse, updateCourse }: {
-            courses: any[]; course: any; setCourse: (course: any) => void;
-            addNewCourse: () => void; deleteCourse: (course: any) => void;
-            updateCourse: () => void;
-        }
-) {
-    const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const [show, setshow] = useState(false)
+{ courses, course, setCourse, addNewCourse,
+  deleteCourse, updateCourse, enrolling, setEnrolling, updateEnrollment }: {
+  courses: any[]; course: any; setCourse: (course: any) => void;
+  addNewCourse: () => void; deleteCourse: (course: any) => void;
+  updateCourse: () => void; enrolling: boolean; setEnrolling: (enrolling: boolean) => void;
+  updateEnrollment: (courseId: string, enrolled: boolean) => void
+}) {
+  const dispatch = useDispatch();
 
-    return (
-        <div id="wd-dashboard">
-            <h1 id="wd-dashboard-title">Dashboard</h1>
-            {currentUser.role == "FACULTY" ? <> <h5>New Course
-                <button className="btn btn-primary float-end"
-                    id="wd-add-new-course-click"
-                    onClick={addNewCourse} > Add </button>
-            </h5>
-                <br />
-                <input defaultValue={course.name} className="form-control mb-2" onChange={(e) => setCourse({ ...course, name: e.target.value })} />
-                <textarea defaultValue={course.description} className="form-control"
-                    onChange={(e) => setCourse({ ...course, description: e.target.value })} />
+  const fallback = "reactjs.jpg";
 
-                <hr /></> : <button className="btn btn-primary float-end"
-                    id="wd-add-new-course-click"
-                    onClick={() => { setshow(!show) }} > Enrollment </button>}
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
 
-            <h2 id="wd-dashboard-published">Published Courses ({(currentUser ? courses
-                : []).length})</h2> <hr />
-            <div id="wd-dashboard-courses" className="row">
-                <div className="row row-cols-1 row-cols-md-5 g-4">
-                    {courses
-                        .map((course) => (
-                            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                                <div className="card rounded-3 overflow-hidden">
-                                    <Link to={`/Kanbas/Courses/${course._id}/Home`}
-                                        className="wd-dashboard-course-link text-decoration-none text-dark" >
-                                        <img src="/images/reactjs.jpg" width="100%" height={160} />
-                                        <div className="card-body">
-                                            <h5 className="wd-dashboard-course-title card-title">
-                                                {course.name} </h5>
-                                            <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
-                                                {course.description} </p>
-                                            <button className="btn btn-primary"> Go </button>
-                                            {currentUser.role == "FACULTY" && <><button onClick={(event) => {
-                                                event.preventDefault();
-                                                deleteCourse(course._id);
-                                            }} className="btn btn-danger float-end"
-                                                id="wd-delete-course-click">
-                                                Delete
-                                            </button>
-                                                <button className="btn btn-warning float-end me-2"
-                                                    onClick={updateCourse} id="wd-update-course-click">
-                                                    Update
-                                                </button></>}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                        ))}
+  function NewCourse() {
+    return <>
+      <h5>New Course
+        <button className="btn btn-primary float-end"
+                id="wd-add-new-course-click"
+                onClick={addNewCourse} > Add </button>
+      </h5><br />
+      <input defaultValue={course.name} className="form-control mb-2"
+             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <textarea defaultValue={course.description} className="form-control"
+                onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
+      <hr />
+    </>;
+  }
+
+  function DeleteUpdate() {
+    return <>
+      <button onClick={(event) => {
+                event.preventDefault();
+                deleteCourse(course._id);
+              }}
+              className="btn btn-danger float-end"
+              id="wd-delete-course-click">
+        Delete
+      </button>
+      <button className="btn btn-warning float-end me-2"
+              onClick={updateCourse} id="wd-update-course-click">
+        Update
+      </button>
+    </>;
+  }
 
 
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      {currentUser.role === "FACULTY" ? <NewCourse/> : <></>}
+
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          {courses
+            .map((course: any) => (
+              <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+                <div className="card rounded-3 overflow-hidden">
+                  <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                        className="wd-dashboard-course-link text-decoration-none text-dark" >
+                    <img src={`/images/${course.image || fallback}`} width="100%" height={160} />
+                    <div className="card-body">
+                      <h5 className="wd-dashboard-course-title card-title">
+                        {enrolling && (
+                           <button onClick={(event) => {
+                                     event.preventDefault();
+                                     updateEnrollment(course._id, !course.enrolled);
+                                    }}
+                                    className={`btn ${ course.enrolled ? "btn-danger" : "btn-success" } float-end`} >
+                             {course.enrolled ? "Unenroll" : "Enroll"}
+                           </button>
+                         )}
+                         {course.name}
+                      </h5>
+                      <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                        {course.description} </p>
+                      <button className="btn btn-primary"> Go </button>
+                      {currentUser.role === "FACULTY" ? <DeleteUpdate/> : <></>}
+                    </div>
+                  </Link>
                 </div>
-            </div>
+              </div>
+            ))}
         </div>
-    );
-}
+      </div>
+    </div>);}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,71 +1,131 @@
 import { Routes, Route, Navigate } from "react-router";
+import { useSelector } from "react-redux";
 import Account from "./Account";
+import Session from "./Account/Session";
+import ProtectedRoute from "./Account/ProtectRoute";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
-import * as userClient from "./Account/client";
+import './styles.css';
+import { useState, useEffect } from "react";
 import * as courseClient from "./Courses/client";
-import { useEffect, useState } from "react";
-import "./styles.css";
-import PeopleTable from "./Courses/People/Table";
-import ProtectedRoute from "./Account/ProtectRoute";
-import { useSelector } from "react-redux";
+import * as userClient from "./Account/client";
+
 export default function Kanbas() {
-  const [courses, setCourses] = useState<any[]>([]);
+
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const fetchCourses = async () => {
+  const [courses, setCourses] = useState<any[]>([]);
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+  const findCoursesForUser = async () => {
     try {
-      const courses = await userClient.findMyCourses();
+      const courses = await userClient.findCoursesForUser(currentUser._id);
       setCourses(courses);
     } catch (error) {
       console.error(error);
     }
   };
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    if (enrolled) {
+      await userClient.enrollIntoCourse(currentUser._id, courseId);
+    } else {
+      await userClient.unenrollFromCourse(currentUser._id, courseId);
+    }
+    setCourses(
+      courses.map((course) => {
+        if (course._id === courseId) {
+          return { ...course, enrolled: enrolled };
+        } else {
+          return course;
+        }
+      })
+    );
+  };
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      const courses = allCourses.map((course: any) => {
+        if (enrolledCourses.find((c: any) => c._id === course._id)) {
+          return { ...course, enrolled: true };
+        } else {
+          return course;
+        }
+      });
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
+
 
   const [course, setCourse] = useState<any>({
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
   });
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  const addNewCourse = async () => {
+    const newCourse = await courseClient.createCourse(course);
+    setCourses([...courses, newCourse]);
   };
   const deleteCourse = async (courseId: string) => {
     const status = await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
 
-  const updateCourse = async () => {
-    await courseClient.updateCourse(course);
-    setCourses(courses.map((c) => {
-        if (c._id === course._id) { return course; }
-        else { return c; }
-    })
-  );};
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
 
+  const testEnrollment = (currentUser: any, params: any) => {
+    const { cid } = params;
+    return courses.some((course: any) => course._id === cid);
+  };
 
   return (
-    <div id="wd-kanbas">
-      <KanbasNavigation />
-      <div className="wd-main-content-offset p-3">
-        <Routes>
-          <Route path="/" element={<Navigate to="Account" />} />
-          <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={  <ProtectedRoute fallback="/Kanbas/Account/Signin"><Dashboard
-            courses={courses}
-            course={course}
-            setCourse={setCourse}
-            addNewCourse={addNewCourse}
-            deleteCourse={deleteCourse}
-            updateCourse={updateCourse} /></ProtectedRoute>} />
- <Route path="Courses/:cid/*" element={  <ProtectedRoute fallback="/Kanbas/Account/Signin"><Courses courses={courses} /></ProtectedRoute>} />
-          <Route path="/Calendar" element={<h1>Calendar</h1>} />
-          <Route path="/Inbox" element={<h1>Inbox</h1>} />
-          <Route path="People" element={<PeopleTable />} />
-        </Routes>
+    <Session>
+      <div id="wd-kanbas">
+        <KanbasNavigation />
+        <div className="wd-main-content-offset p-3">
+          <Routes>
+            <Route path="/" element={<Navigate to="Account" />} />
+            <Route path="/Account/*" element={<Account />} />
+            <Route path="/Dashboard" element={
+              <ProtectedRoute fallback="/Kanbas/Account/Signin">
+                <Dashboard
+                  courses={courses} course={course} setCourse={setCourse}
+                  addNewCourse={addNewCourse} deleteCourse={deleteCourse} updateCourse={updateCourse}
+                  enrolling={enrolling} setEnrolling={setEnrolling}  updateEnrollment={updateEnrollment}/>
+              </ProtectedRoute>
+            } />
+            <Route path="/Courses/:cid/*" element={
+              <ProtectedRoute fallback="/Kanbas/Account/Signin">
+                <ProtectedRoute fallback="/Kanbas/Dashboard" pred={testEnrollment}>
+                  <Courses courses={courses}/>
+                </ProtectedRoute>
+              </ProtectedRoute>
+            } />
+            <Route path="/Calendar" element={<h1>Calendar</h1>} />
+            <Route path="/Inbox" element={<h1>Inbox</h1>} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Session>
   );
 }
